755141c7c2c58095e15c966a64897eda9347b002
NgoQuyTruong <37119899+Chuong1928@users.noreply.github.com>
2020-07-17 21:03:38 +0700

    Update control.php
    update ajax

--- control.php
+++ control.php
@@ -8,9 +8,6 @@
             </div> 
              
              <div class="ques_conntent">
-           <!--    <h3 id="question">Khái niệm “Làn đường” được hiểu như thế nào là đúng?
-              Khái niệm “Làn đường” được hiểu như thế nào là đúng?
-            Khái niệm “Làn đường” được hiểu như thế nào là đúng?</h3> -->
             <?php 
                  require_once('include/function.php');
                include_once('./Connect/config.php');
@@ -18,12 +15,12 @@
                $sql="SELECT * from cauhoi where MaCh=$mach";
                $result=$conn->query($sql);
               $row = mysqli_fetch_assoc($result);
-              $dapanchon=$row['DapAnChon'];
+              $dapanchon=$row['Trangthai'];
               echo '<h3 id="question"> '.$row['NoiDung'].' </h3>';
                ?>
              
             </div>
-            <?php 
+      <!--       <?php 
                 if($_SERVER['REQUEST_METHOD']=="POST"){
                      if(isset($_POST['dapan'])){
                        $dapan=$_POST['dapan'];
@@ -31,7 +28,7 @@
                      else{
                         $dapan=false;
                      }
-                    // $dapan=$_POST['dapan'];
+                    
                    if(empty($dapan)){
                       $error ="CHỌN ĐÁP ÁN ĐI BẠN ƠI";
                    }
@@ -40,12 +37,10 @@
                    }
                 }
 
-                  
 
-             ?>
+             ?> -->
             <div class="anwser">
                <?php 
-            
                $mach=$_GET['index'];
                $sql="SELECT * from dapan  where MaCh=$mach";
                $result=$conn->query($sql);
@@ -53,15 +48,15 @@
                
                   ?>
             
-                      <input id="ckbox" TYPE="RADIO"  VALUE="A" NAME="dapan"  >
+                      <input id="ck0" TYPE="RADIO"  VALUE="0" NAME="dapan" <?php if(isset($dapanchon) && $dapanchon=='0')  echo "checked='checked' " ?> >
                       A. <?php  echo $row['A']; ?><BR>
 
-                      <input id="ckbox" TYPE="RADIO" VALUE="B" NAME="dapan" >
+                      <input id="ck1" TYPE="RADIO" VALUE="1" NAME="dapan" <?php if(isset($dapanchon) && $dapanchon=='1')  echo "checked='checked' " ?>>
                       B. <?php  echo $row['B']; ?><BR>
 
-                      <input id="ckbox" TYPE="RADIO"  VALUE="C" NAME="dapan" >
+                      <input id="ck2" TYPE="RADIO"  VALUE="2" NAME="dapan" <?php if(isset($dapanchon) && $dapanchon=='2')  echo "checked='checked' " ?> >
                       C. <?php  echo $row['C']; ?><BR>
-                      <input id="ckbox" TYPE="RADIO" VALUE="D" NAME="dapan" >
+                      <input id="ck3" TYPE="RADIO" VALUE="3" NAME="dapan" <?php if(isset($dapanchon) && $dapanchon=='3')  echo "checked='checked' " ?> >
                       D. <?php  echo $row['D']; ?><BR>
                       <p style="color: red"> <?php if(!empty($error)) echo $error; ?></p>
                   
@@ -72,40 +67,136 @@
          <?php 
           $num=$_GET['index'];
          if ($num==1) {
-           echo '<input class="btn btn-primary list-q" type="button" index=" '.$num.' " value="Câu Trước">';
+           echo '<input class="btn btn-primary list-q" type="button"  truongdz=" '.$num.' " value="Câu Trước">';
          } else {
           $id=(int)$_GET['index']-1;
-            echo '<input class="btn btn-primary list-q" type="button" index=" '.$id.' " value="Câu Trước ">';
+            echo '<input class="btn btn-primary list-q back insert_b"  type="button"  truongdz=" '.$id.' " value="Câu Trước ">';
          }
-         
+  
           ?>
          <?php 
           $id=(int)$_GET['index']+1;
-          echo '<input class="btn btn-primary list-q" type="button" index=" '.$id.' " value="Câu Sau">';
+          echo '<input id="btnsubmit" class="btn btn-primary list-q insert next"   type="button" truongdz=" '.$id.' " value="Câu Sau">';
           ?>
-          <script type="text/javascript">
+  <script type="text/javascript">
+  $(document).ready(function(){
+ $('.insert').click(function(){
+   // var el = this;
+   // var id = this.id;
+   // var splitid = id.split("_");
+   // // Delete id
+   // //dapancon
+   var index = $(this).attr("truongdz")-1;
+    var vall=$('input[type="RADIO"]:checked');
+    // alert(index);
+    
+    // alert(vall.val());
+   // AJAX Request
+   $.ajax({
+     url: 'insert.php',
+     type: 'POST',
+     data: { id:index,da:vall.val() },
+     success:function(response){
+      if(response ==1){
+
+      }else{
+
+    alert('Lỗi');
+    alert(response);
+    //alert(id);
+      }
+
+    }
+   });
+
+ });
+
+});
+
+</script>
+  <script type="text/javascript">
+  $(document).ready(function(){
+ $('.insert_b').click(function(){
+   // var el = this;
+   // var id = this.id;
+   // var splitid = id.split("_");
+   // // Delete id
+   // //dapancon
+   var index = $(this).attr("truongdz")-(-1);
+    var vall=$('input[type="RADIO"]:checked');
+    // alert(index);
+    
+    // alert(vall.val());
+   // AJAX Request
+   $.ajax({
+     url: 'insert.php',
+     type: 'POST',
+     data: { id:index,da:vall.val()},
+     success:function(response){
+      if(response ==1){
+
+      }else{
+
+    alert('Lỗi');
+    alert(response);
+    //alert(id);
+      }
+
+    }
+   });
+
+ });
+
+});
+
+</script>
+ <script type="text/javascript">
   jQuery(document).ready(function() {
-    $('.list-q').click(function() {
+    $('.next').click(function() {
   /* Act on the event */
-  var index = $(this).attr("index");
+  var index = $(this).attr("truongdz");
   $.ajax({
     url: 'control.php',
     type: 'GET',
     data: {index: index},
     success:function(data){
+      //alert("oke");
       $('.question').html(data);
-        // alert(data);
-
     }
    });
 
 });
   });
+
 </script>
+ <script type="text/javascript">
+  jQuery(document).ready(function() {
+    $('.back').click(function() {
+  /* Act on the event */
+  var index = $(this).attr("truongdz");
+  $.ajax({
+    url: 'control.php',
+    type: 'GET',
+    data: {index: index},
+    success:function(data){
+      //alert("oke");
+      $('.question').html(data);
+    }
+   });
+
+});
+  });
+
+</script>
+<div id="test"></div>
  <script>
         $(document).ready(function(){
-          // alert("helloworld");
-          $("input[name='chuyen_radio_tab_or_button']:eq(0) ").attr("checked","checked");
+        var vall=$('input[type="RADIO"]:checked');
+          if(vall==null){
+            alert("bạn chưa chọn");
+          }
+        // alert("helloworld");
+       //   $("input[name='dapan']:eq(0) ").attr("checked","checked");
         });
       </script>
             </div>
